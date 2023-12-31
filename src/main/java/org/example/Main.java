22ae108342d6af4e6f2a58a14b7d51e89390ef5d
Thomas Barth <thbar6999@uib.no>
2023-12-31 22:54:42 +0100

    V1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         CreateTable testTable = new CreateTable();
-        ArrayList<String>ids = FindIDs.findIds(8650,9650);
-        ids.addAll(FindIDs.findIds(9651,10600));
+        ArrayList<String>ids = FindIDs.findIds(8000,10700); //2018 - 2023
         testTable.updateTable(ids);
-        System.out.println("Finding stats from: "+ids.size()+" tournaments");
         System.out.println(testTable.fullTable.get("Thomas Barth"));
+
     }
 }
